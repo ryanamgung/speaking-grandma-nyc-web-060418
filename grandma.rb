e612d61f16d2a049b34bde16fc900cd7fdf1a0e4
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-01 02:55:36 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,9 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if(phrase == "I LOVE YOU GRANDMA!")
-    
-  elsif(phrase.upcase == phrase){
-    "NO, NOT SINCE 1938!"
-  }
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase.upcase == phrase
+    return "NO, NOT SINCE 1938!"
+  else 
+    return "HUH?! SPEAK UP, SONNY!"
+  end
 end
